4f827d300e250f059c7de0c7a150be6f21411010
Salameboy <iamperdomo@gmail.com>
2019-09-09 16:12:36 -0300

    Agregado: - Plugin firebase - init push notifications

--- app/app.js
+++ app/app.js
@@ -2,6 +2,27 @@ import Vue from "nativescript-vue";
 
 import Home from "./components/Home";
 
+var firebase = require("nativescript-plugin-firebase");
+
+    firebase.init({
+      showNotifications: true,
+      showNotificationsWhenInForeground: true,
+
+      onPushTokenReceivedCallback: (token) => {
+        console.log('[Firebase] onPushTokenReceivedCallback:', { token });
+      },
+
+      onMessageReceivedCallback: (message) => {
+        console.log('[Firebase] onMessageReceivedCallback:', { message });
+      }
+    })
+      .then(() => {
+        console.log('[Firebase] Initialized');
+      })
+      .catch(error => {
+        console.log('[Firebase] Initialize', { error });
+      });
+
 new Vue({
 
     template: `
